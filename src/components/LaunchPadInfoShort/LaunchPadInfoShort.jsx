1f2252deee37f393e40178b67ac0163028419b13
Roman Petrov <rpetrov.ru@gmail.com>
2019-11-24 00:33:20 +0300

    eslint fix

--- src/components/LaunchPadInfoShort/LaunchPadInfoShort.jsx
+++ src/components/LaunchPadInfoShort/LaunchPadInfoShort.jsx
@@ -6,32 +6,30 @@ import withLoader from '../../hoc/withLoader';
 import { fetchLaunchPadsIfNeeded } from '../../redux/actions/launchPads';
 import { launchPadById } from '../../redux/reducers/launchPads';
 
-export const LaunchPadInfoShort = ({ launchPad, launchPadId }) => {
-  return (
-    <div>
-      <h2>{launchPad.site_name_long}</h2>
-      <dl>
-        <dt>Status:</dt>
-        <dd>{launchPad.status}</dd>
+export const LaunchPadInfoShort = ({ launchPad, launchPadId }) => (
+  <div>
+    <h2>{launchPad.site_name_long}</h2>
+    <dl>
+      <dt>Status:</dt>
+      <dd>{launchPad.status}</dd>
 
-        <dt>Vehicles launched</dt>
-        <dd>{launchPad.vehicles_launched.join(', ')}</dd>
+      <dt>Vehicles launched</dt>
+      <dd>{launchPad.vehicles_launched.join(', ')}</dd>
 
-        <dt>Attempted launches</dt>
-        <dd>{launchPad.attempted_launches}</dd>
+      <dt>Attempted launches</dt>
+      <dd>{launchPad.attempted_launches}</dd>
 
-        <dt>Successful launches</dt>
-        <dd>{launchPad.successful_launches}</dd>
-      </dl>
+      <dt>Successful launches</dt>
+      <dd>{launchPad.successful_launches}</dd>
+    </dl>
 
-      <p>
-        <Link to={`/catalog/launch_pads/${launchPadId}`}>
-          Read more!
-        </Link>
-      </p>
-    </div>
-  );
-};
+    <p>
+      <Link to={`/catalog/launch_pads/${launchPadId}`}>
+        Read more!
+      </Link>
+    </p>
+  </div>
+);
 LaunchPadInfoShort.propTypes = {
   launchPad: PropTypes.instanceOf(Object).isRequired,
   launchPadId: PropTypes.number.isRequired,
