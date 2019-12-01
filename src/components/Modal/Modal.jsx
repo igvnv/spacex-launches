9ac7ab154096c5faeae9b46dcf1eee5d058f8534
Roman Petrov <rpetrov.ru@gmail.com>
2019-12-01 14:57:46 +0300

    modal in portal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
-const Modal = ({ children, onClose }) => (
-  <>
-    <div className="modal-background" />
-    <div className="modal">
-      <button
-        className="modal__close-button"
-        type="button"
-        aria-label="Close window"
-        onClick={onClose}
-      />
-      <div className="modal__body">
-        {children}
-      </div>
-    </div>
-  </>
-);
+import usePortal from '../../hooks/usePortal';
+
+const Modal = ({ children, onClose }) => {
+  const target = usePortal('modal');
+
+  return createPortal(
+    (
+      <>
+        <div className="modal-background" />
+        <div className="modal">
+          <button
+            className="modal__close-button"
+            type="button"
+            aria-label="Close window"
+            onClick={onClose}
+          />
+          <div className="modal__body">
+            {children}
+          </div>
+        </div>
+      </>
+    ),
+    target,
+  );
+};
 Modal.propTypes = {
   children: PropTypes.element.isRequired,
   onClose: PropTypes.func.isRequired,
